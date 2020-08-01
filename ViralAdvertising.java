5f73bab4c590eb71ae0fc8bfc1f953129b91f988
Pvsaraf <64657857+Pvsaraf@users.noreply.github.com>
2020-08-01 19:56:14 +0530

    Created ViralAdvertising.java
    HackerLand Enterprise is adopting a new viral advertising strategy. When they launch a new product, they advertise it to exactly  people on social media.
    
    On the first day, half of those  people (i.e., ) like the advertisement and each shares it with  of their friends. At the beginning of the second day,  people receive the advertisement.
    
    Each day,  of the recipients like the advertisement and will share it with  friends on the following day. Assuming nobody receives the advertisement twice, determine how many people have liked the ad by the end of a given day, beginning with launch day as day .

---
+++ ViralAdvertising.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class ViralAdvertising {
+
+    // Complete the viralAdvertising function below.
+    static int viralAdvertising(int n) {
+        int res=5,sum=0;
+        for(int i=1;i<=n;i++)
+        {
+            sum+=(res/2);
+            res=(res/2)*3;           
+        }
+        return sum;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int result = viralAdvertising(n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
